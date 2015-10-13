3da24f42d1978eddccb1b6d55f45889d1f065eab
sadath1810 <sadath1810@yahoo.com>
2015-10-13 14:06:46 +0530

    AngularJS Product Store with Web API 2
    First Cut

---
+++ APM/APMSln/APM.Web/common/services/common.services.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Deb on 8/21/2014.
+ */
+(function () {
+    "use strict";
+
+    angular
+        .module("common.services",
+                ["ngResource"])
+        .constant("appSettings",
+        {
+            serverPath: "http://localhost:60672"
+        });
+}());
+
